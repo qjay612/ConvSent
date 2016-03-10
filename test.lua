901f389f8526b1e84eb85b0bbd8b459881554a93
ppaanngggg <ppaanngggg@gmail.com>
2016-03-10 14:54:18 +0800

    train and test

---
+++ test.lua
@@ -0,0 +1,45 @@
+dofile('ConvSent.lua')
+
+require 'cutorch'
+require 'pnn'
+
+cutorch.setDevice(5)
+
+dataset = torch.load('./test_dataset')
+
+for i = 1,#dataset do
+    local size = dataset[i][1][1]:size()
+    if size[1] < params.KERNEL_SIZE[#params.KERNEL_SIZE] then
+        local tmp = torch.zeros(params.KERNEL_SIZE[#params.KERNEL_SIZE], size[2])
+        tmp:narrow(1,1,size[1]):copy(dataset[i][1][1])
+        dataset[i][1][1] = tmp
+    end
+    size = dataset[i][1][1]:size()
+    dataset[i][1] = dataset[i][1][1]:reshape(1,size[1],size[2])
+end
+dataset = pnn.recursiveCuda(dataset)
+
+model = torch.loadobj('model')
+model = model.module
+model:evaluate()
+model:cuda()
+
+correctNum = 0
+
+for i = 1,#dataset do
+    output = model:forward(dataset[i][1])
+    maxIndex = 0
+    maxValue = -1
+    for j = 1,output:size()[2] do
+        if output[1][j] > maxValue then
+            maxValue = output[1][j]
+            maxIndex = j
+        end
+    end
+    print(maxIndex, dataset[i][2], output[1][dataset[i][2]])
+    if maxIndex == dataset[i][2] then
+        correctNum = correctNum + 1
+    end
+end
+
+print(correctNum / #dataset * 100)
--- train.lua
+++ train.lua
@@ -1,12 +1,20 @@
 dofile './ConvSent.lua'
 
 require 'pnn'
+require 'optim'
 
 dataset = torch.load('./train_dataset')
 
 dataList = {}
 for i = 1,#dataset do
-    dataset[i][1] = dataset[i][1][1]
+    local size = dataset[i][1][1]:size()
+    if size[1] < params.KERNEL_SIZE[#params.KERNEL_SIZE] then
+        local tmp = torch.zeros(params.KERNEL_SIZE[#params.KERNEL_SIZE], size[2])
+        tmp:narrow(1,1,size[1]):copy(dataset[i][1][1])
+        dataset[i][1][1] = tmp
+    end
+    size = dataset[i][1][1]:size()
+    dataset[i][1] = dataset[i][1][1]:reshape(1,size[1],size[2])
     if dataList[dataset[i][2]] then
         dataList[dataset[i][2]] = dataList[dataset[i][2]] + 1
     else
